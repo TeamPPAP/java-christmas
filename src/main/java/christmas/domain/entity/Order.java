ee0980313fe2b6bc3b5a95ad389da8de3c075abd
kimsunwook <kckk116560@gmail.com>
2025-10-28 21:15:25 +0900

    feat:주문 도메인 기능 추가 -상품별 갯수 맵핑 메서드 추가 -주문내역에 따른 총 가격 리턴 메서드 추가 -주문 도메인 테스트케이스 추가

--- src/main/java/christmas/domain/entity/Order.java
+++ src/main/java/christmas/domain/entity/Order.java
@@ -1,4 +1,40 @@
 package christmas.domain.entity;
 
+import christmas.domain.enums.MenuItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Order {
+    Map<MenuItem, Integer> quantityByMenuItem;
+
+    //TODO: 정적팩토리 메서드로 변환에 대해 검토
+    //TODO: stream 적용해서 바꿔보기
+    public void parseMenuList(String menuLine) {
+        String menuLineTrimed = menuLine.replace(" ", "");
+        String[] menuAndStockArray = menuLineTrimed.split(",");
+        Map<MenuItem, Integer> menuQuantityMap = new HashMap<MenuItem, Integer>();
+
+        for (String s : menuAndStockArray) {
+           String[] a = s.split("-");
+           MenuItem key = MenuItem.of(a[0]);
+           int count = Integer.parseInt(a[1]);
+           menuQuantityMap.put(key, count);
+        }
+
+        if(menuQuantityMap.size() == 0)
+            throw new IllegalArgumentException("주문이 정상적으로 이루어지지않았습니다.");
+
+        quantityByMenuItem = menuQuantityMap;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (MenuItem menuItem : quantityByMenuItem.keySet()) {
+            totalPrice += menuItem.getPrice() * quantityByMenuItem.get(menuItem);
+        }
+        return totalPrice;
+    }
+
+
 }
---
+++ src/test/java/christmas/domain/entity/OrderTest.java
@@ -0,0 +1,28 @@
+package christmas.domain.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void parseMenuList() {
+        //given
+        Order order = new Order();
+        order.parseMenuList("타파스-1,제로콜라-1");
+    }
+
+    @Test
+    void getTotalMenuPrice() {
+        //given
+        Order order = new Order();
+        order.parseMenuList("타파스-1,제로콜라-1");
+
+        int totalPrice = order.getTotalMenuPrice();
+        Assertions.assertTrue(totalPrice > 0, totalPrice + " 원");
+    }
+
+
+}
