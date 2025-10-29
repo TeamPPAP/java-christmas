7ac12d0fe11553d5335e7bff2ca6c4e1cfab2ef8
kimsunwook <kckk116560@gmail.com>
2025-10-29 20:19:43 +0900

    feat: View 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,16 @@
 package christmas.view;
+import java.util.Scanner;
 
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public int getVisitDate() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public String getOrderDetails() {
+        return scanner.nextLine();
+    }
+
+
 }
